587e19f548230345f12bef85eaa4da13fda6a7c0
nemetsky <nemetsky.sergey@gmail.com>
2025-08-26 21:02:38 +0300

    add_10.2

---
+++ examples/module/module_check_passwd_01.py
@@ -0,0 +1,23 @@
+﻿
+def check_passwd(username, password, *, min_len=8, check_numbers=False):
+    if len(password) < min_len:
+        return False
+    elif username.lower() in password.lower():                      
+        return False
+    elif check_numbers and len(set("0123456789") & set(password)) < 3:  
+        return False
+    else:
+        return True
+
+data = [                               
+    ["user1", "jsadhkj567asd"],
+    ["user2", "20"],
+    ["user3", "user3jsadhkjasd"],                      
+]
+
+# print(__name__)                                       # можно посмотреть что содержится в переменной __name__ при использовании импорта в другом скрипте
+
+if __name__ == "__main__":                              # значит выполнить код, если модуль не импортируется,  __main__ - специальная переменная
+    print("Тестовая проверка")                          # Если запустит этот скрпит напрямую, то эти строки выполняться
+    print(check_passwd("user1", "hbdsfdsf32782309"))
+    print(check_passwd("user2", "hbds309"))
---
+++ examples/module/module_check_passwd_use_01.py
@@ -0,0 +1,20 @@
+﻿from pprint import pprint
+from module_check_passwd_01 import check_passwd, data           # импортируем функцию и переменную из собственный модуль, но при спользовании check_passwd весь модуль module_check_passwd_01 выполниться
+                                                                # поэтому в модуле module_check_passwd_01 используем переменную __name__ (см. там), чтобы использовать только функции из того модуля
+
+# import module_check_passwd_01   # или так импортировать тогда обращаться к фуккции надо так:  module_check_passwd_01.check_passwd, а к переменной module_check_passwd_01.data                             
+                                                                
+def check_user_list(user_passwd_data, **kwargs):      
+    correct_users = []                     
+    wrong_users = []
+    for username, password in user_passwd_data:     
+        check = check_passwd(username, password, **kwargs)  
+        if check:                                  
+            correct_users.append(username)     
+        else:
+            wrong_users.append(username)
+    return correct_users, wrong_users         
+
+ok, no_ok = check_user_list(data, min_len=4, check_numbers=True) 
+pprint(ok)                   
+pprint(no_ok)
