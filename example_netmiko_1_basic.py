dc9bd71e8abcda6d8ebeb60d0f65d0c5a5fbb603
nemetsky <nemetsky.sergey@gmail.com>
2025-10-14 21:06:09 +0300

    add_17.2

--- example_netmiko_1_basic.py
+++ example_netmiko_1_basic.py
@@ -4,21 +4,22 @@ from pprint import pprint
 
 def send_show_command(ip, user, password, command):
     print(f"Подключаюсь к {ip}")
-    with netmiko.ConnectHandler(device_type="cisco_ios", timeout=5, host=ip, username=user, password=password) as ssh:
-        output = ssh.send_command(command)
-        return output
+    try:
+        with netmiko.ConnectHandler(device_type="cisco_ios", timeout=5, host=ip, username=user, password=password) as ssh:
+            output = ssh.send_command(command)
+            return output
+    except netmiko.exceptions.NetmikoTimeoutException:
+        print(f"Не получилось подключиться к {ip}")
+    except netmiko.exceptions.NetmikoAuthenticationException as error:
+        print(error)
+    # except ValueError as error:       # это исключение если используем парольна enable и неправильно ввели его (можно объеденить с предыдущим исключением т.к. там используем тоже as error)
+        # print(error)
 
 if __name__ == "__main__":                                                             
-    ip_list = ["10.11.10.107"]
+    ip_list = ["10.11.10.107", "192.168.11.11"]
     password = getpass.getpass()
     for ip in ip_list:
-        out = send_show_command(ip, "nemetskiy-sv", password, "sh int descr")     
+        out = send_show_command(ip, "nemetskiy-sv", password, "sh clock")     
         pprint(out, width=120)
 
-    
-"""
-device_params = {"device_type": "cisco_ios", 
-                 "host": "10.11.10.107",
-                 "username": "nemetskiy-sv",
-                 "password": "SegNemVlad#3"}
-"""                 
+   
---
+++ example_netmiko_1_basic_device_dict.py
@@ -0,0 +1,27 @@
+﻿import netmiko
+import yaml
+import getpass
+from pprint import pprint
+
+def send_show_command(device_params, password, command):    # пароль в функцию можно отдельно передать, чтобы не хранить в файле со словарями
+    ip = device_params["host"]                              # можно так из словаря достать ip-адрес
+    print(f"Подключаюсь к {ip}")
+    try:
+        with netmiko.ConnectHandler(password=password, **device_params) as ssh:
+            output = ssh.send_command(command)
+            return output
+    except netmiko.exceptions.NetmikoTimeoutException:
+        print(f"Не получилось подключиться к {ip}")
+    except netmiko.exceptions.NetmikoAuthenticationException as error:
+        print(error)
+
+if __name__ == "__main__":                                                             
+    with open("devices.yaml") as f:                         # в файле "devices.yaml" у нас содержится список словарей с параметрами подключения (тип, адрес, пользователь и т.д)
+        devices = yaml.safe_load(f)                         # загружаем список словарей в переменную
+        # print(devices)                     
+    password = getpass.getpass()                            # пароль отдельно запрашиваем
+    for dev in devices:                                     # перебираем словари с нашими устройствами
+        out = send_show_command(dev, password, "sh clock")        
+        pprint(out, width=120)
+
+   
